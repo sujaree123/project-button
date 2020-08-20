8667c5f414256a8f44174c80d0d4f4d0b4b95313
sujaree123 <sujaree.k2543@gmail.com>
2020-08-20 20:38:43 +0700

    index

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,147 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-wrapper-first">
+      <section class="service-area">
+        <div class="container">
+          <HelloWorld msg="Dogs on the beach" />
+          <div class="row">
+            <div class="col-md-6">
+              <div class="single-service" style="background: url(img/1.jpg);">
+                <div class="overlay overlay-content justify-content-center">
+                  <h1>
+                    <i class="fas fa-hand-point-right text-secondary"></i>
+                    {{total1}}
+                    <i class="fas fa-hand-point-left text-secondary"></i>
+                  </h1>
+                  <CsButton @cc="sum" />
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="single-service" style="background: url(img/2.jpg);">
+                <div class="overlay overlay-content justify-content-center">
+                  <h1>
+                    <i class="fas fa-hand-point-right text-secondary"></i>
+                    {{total2}}
+                    <i class="fas fa-hand-point-left text-secondary"></i>
+                  </h1>
+                  <CsButton @cc="sum1" />
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="single-service" style="background: url(img/3.jpg);">
+                <div class="overlay overlay-content justify-content-center">
+                  <h1>
+                    <i class="fas fa-hand-point-right text-secondary"></i>
+                    {{total3}}
+                    <i class="fas fa-hand-point-left text-secondary"></i>
+                  </h1>
+                  <CsButton @cc="sum2" />
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="single-service" style="background: url(img/4.jpg);">
+                <div class="overlay overlay-content justify-content-center">
+                  <h1>
+                    <i class="fas fa-hand-point-right text-secondary"></i>
+                    {{total4}}
+                    <i class="fas fa-hand-point-left text-secondary"></i>
+                  </h1>
+                  <CsButton @cc="sum3" />
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="single-service" style="background: url(img/5.jpg);">
+                <div class="overlay overlay-content justify-content-center">
+                  <h1>
+                    <i class="fas fa-hand-point-right text-secondary"></i>
+                    {{total5}}
+                    <i class="fas fa-hand-point-left text-secondary"></i>
+                  </h1>
+                  <CsButton @cc="sum4" />
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="single-service" style="background: url(img/6.jpg);">
+                <div class="overlay overlay-content justify-content-center">
+                  <h1>
+                    <i class="fas fa-hand-point-right text-secondary"></i>
+                    {{total6}}
+                    <i class="fas fa-hand-point-left text-secondary"></i>
+                  </h1>
+                  <CsButton @cc="sum5" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CsButton.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+  },
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+      total3: 0,
+      total4: 0,
+      total5: 0,
+      total6: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+    sum2(counterValue) {
+      this.total3 = counterValue;
+    },
+    sum3(counterValue) {
+      this.total4 = counterValue;
+    },
+    sum4(counterValue) {
+      this.total5 = counterValue;
+    },
+    sum5(counterValue) {
+      this.total6 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Itim", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  background-image: url("/img/111.jpg");
 }
 </style>
---
+++ src/components/CsButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="genric-btn primary circle ml-3 mt-10">
+      <span class="text-black fas fa-thumbs-up"> Like</span>
+    </button>
+
+    <button v-on:click="downFunction" class="genric-btn success circle ml-3 mt-10">
+      <span class="text-black fas fa-laugh-beam"> Takecare</span>
+    </button>
+
+    <button v-on:click="loveFunction" class="genric-btn info circle ml-3 mt-10">
+      <span class="text-black fas fa-heart"> Love</span>
+    </button>
+
+    <button v-on:click="dislikeFunction" class="genric-btn circle warning ml-3 mt-10">
+      <span class="text-black fas fa-thumbs-down"> Dislike</span>
+    </button>
+
+    <button v-on:click="ResetFunction" class="genric-btn danger circle ml-3 mt-10">
+      <span class="text-black fas fa-redo-alt"> Reset</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CSButton",
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 5;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += 20;
+      this.$emit("cc", this.counter);
+    },
+
+    loveFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    dislikeFunction: function () {
+      this.counter -= 15;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
